f782a842ceea034870766e33cfa1c7188bbf37f7
abhichy30@gmail.com <abhichy30@gmail.com>
2022-04-10 01:07:50 +0600

    ros_package

--- src/topic01_basics/talker_listener/listener.cpp
+++ src/topic01_basics/talker_listener/listener.cpp
@@ -1,6 +1,6 @@
 /*
- * Author: Anis Koubaa for Gaitech EDU
- * Year: 2016
+ * Author: Avijit Chowdhury
+ * Year: 2022
  *
  */
 
@@ -8,25 +8,23 @@
 #include "std_msgs/String.h"
 
 // Topic messages callback
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
-    ROS_INFO("[Listener] I heard: [%s]\n", msg->data.c_str());
+void chatterCallback(const std_msgs::String::ConstPtr &msg) {
+  ROS_INFO("[Listener] I heard: [%s]\n", msg->data.c_str());
 }
 
-int main(int argc, char **argv)
-{
-    // Initiate a new ROS node named "listener"
-	ros::init(argc, argv, "listener_node");
-	//create a node handle: it is reference assigned to a new node
-	ros::NodeHandle node;
+int main(int argc, char **argv) {
+  // Initiate a new ROS node named "listener"
+  ros::init(argc, argv, "listener_node");
+  // create a node handle: it is reference assigned to a new node
+  ros::NodeHandle node;
 
+  // Subscribe to a given topic, in this case "chatter".
+  // chatterCallback: is the name of the callback function that will be executed
+  // each time a message is received.
+  ros::Subscriber sub = node.subscribe("chatter", 1000, chatterCallback);
 
-    // Subscribe to a given topic, in this case "chatter".
-	//chatterCallback: is the name of the callback function that will be executed each time a message is received.
-    ros::Subscriber sub = node.subscribe("chatter", 1000, chatterCallback);
+  // Enter a loop, pumping callbacks
+  ros::spin();
 
-    // Enter a loop, pumping callbacks
-    ros::spin();
-
-    return 0;
+  return 0;
 }
